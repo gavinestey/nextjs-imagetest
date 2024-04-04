e4e850512389e4ae381b519c164611556802ea6b
Gavin Estey <gavin@estey.com>
2024-04-04 10:18:13 -0700

    test remote SC image

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,12 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <Image
+          src="https://darl-p-001.sitecorecontenthub.cloud/api/public/content/6c25638ae9e746fab8b546b23ce86e5a?v=bf6e7679"
+          width={1200}
+          height={533}
+          alt="Test image"
+        />
       </div>
     </main>
   );
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        loader: 'custom',
+        LoaderFile: './source/loader.js',
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'darl-p-001.sitecorecontenthub.cloud',
+                port: '',
+                pathname: '/api/public/content/**',
+            },
+        ],
+    },
+};
 
 export default nextConfig;
