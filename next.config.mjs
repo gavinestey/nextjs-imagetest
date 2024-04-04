428b29177ab76af82006055f78e3d0d24de704a9
Gavin Estey <gavin@estey.com>
2024-04-04 10:22:20 -0700

    remove loader

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+//loader: 'custom',
+//loaderFile: './source/loader.js',
+
 const nextConfig = {
     images: {
-        loader: 'custom',
-        loaderFile: './source/loader.js',
         remotePatterns: [
             {
                 protocol: 'https',
