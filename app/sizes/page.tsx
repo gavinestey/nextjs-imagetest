42fdebf9d36a87a1778befc369848b515245d226
Gavin Estey <gavin@estey.com>
2024-04-09 21:00:36 -0700

    objectFit

--- app/sizes/page.tsx
+++ app/sizes/page.tsx
@@ -5,10 +5,11 @@ export default function Sizes() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <Image
-          fill
           src="https://darl-p-001.sitecorecontenthub.cloud/api/public/content/6c25638ae9e746fab8b546b23ce86e5a?v=bf6e7679"
           alt="Test responsive image"
-          sizes="(max-width: 500px) 100vw, 25vw"
+          fill
+          sizes="300px"
+          style={{ objectFit: 'contain' }}
         />
       </div>
     </main>
