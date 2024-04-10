233ab346411abf891539a895c06f52b306dfc89b
Gavin Estey <gavin@estey.com>
2024-04-09 20:55:12 -0700

    adding sizes

--- app/sizes/page.tsx
+++ app/sizes/page.tsx
@@ -5,10 +5,10 @@ export default function Sizes() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <Image
+          fill
           src="https://darl-p-001.sitecorecontenthub.cloud/api/public/content/6c25638ae9e746fab8b546b23ce86e5a?v=bf6e7679"
-          width={1200}
-          height={533}
-          alt="Test image"
+          alt="Test responsive image"
+          sizes="(max-width: 500px) 250px, 500px"
         />
       </div>
     </main>
